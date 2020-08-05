7b382b9a327a890233e186b113d5493cdb2d1983
pRob3 <hundraprocent@me.com>
2020-08-05 17:35:20 +0200

    Updates

---
+++ precache-manifest.a4d6126a7d4a80f5b458890753c317ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97f082d40bbb70f52b4e883e1d9afdc5",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "7bf1a7132fddf04d2f04",
+    "url": "/facerecognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ed7448f9ae3cbba84bb2",
+    "url": "/facerecognition/static/css/main.7b271d28.chunk.css"
+  },
+  {
+    "revision": "7bf1a7132fddf04d2f04",
+    "url": "/facerecognition/static/js/2.46ff8677.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facerecognition/static/js/2.46ff8677.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed7448f9ae3cbba84bb2",
+    "url": "/facerecognition/static/js/main.1c4af8e8.chunk.js"
+  },
+  {
+    "revision": "fa6a11a9cd7eebd80802",
+    "url": "/facerecognition/static/js/runtime-main.1ad3eaac.js"
+  }
+]);
