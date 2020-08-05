8c78fa4060c7860c9ecddf2c77e6c0fcc57a9c9b
pRob3 <hundraprocent@me.com>
2020-08-05 19:57:57 +0200

    Image styling

--- src/App.js
+++ src/App.js
@@ -53,7 +53,8 @@ class App extends Component {
       .then(
         function (response) {
           // do something with response
-          console.log(response);
+          console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+          // response.outputs[0].data.regions.[0].region_info.bounding_box
         },
         function (err) {
           // there was an error
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
 
-// API Docs:
-// https://www.clarifai.com/models/face-detection-image-recognition-model-a403429f2ddf4b49b307e318f00e528b-detection#documentation
+/* API Docs:
+ https://www.clarifai.com/models/face-detection-image-recognition-model-a403429f2ddf4b49b307e318f00e528b-detection#documentation
+ https://github.com/Clarifai/clarifai-javascript
+*/
 
-const FaceRecognition = ({imageUrl}) => {
+const FaceRecognition = ({ imageUrl }) => {
+    // console.log('check', imageUrl)
     return (
-        <div className='center '>
-            <img src={imageUrl} alt='Scanned' />
+        <div className='center ma'>
+            <div className='absolute mt2'>
+                <img src={imageUrl} alt='Scanned' width='500px' height='auto' />
+            </div>
         </div>
     );
 }
