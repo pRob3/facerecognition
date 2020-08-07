8e80fd4bb333425786e0f28bccfbe218baefb02b
pRob3 <hundraprocent@me.com>
2020-08-07 16:32:59 +0200

    Added Sign in page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
+import SignIn from './components/SignIn/SignIn';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
@@ -84,7 +85,7 @@ class App extends Component {
           params={particlesOptions}
         />
         <Navigation />
-
+        <SignIn />
         <Logo />
         <Rank />
         <ImageLinkForm
